660dbb69a5da05e9ebdd967d710042a9660f2bb2
Shawn Ferris <sferris@localhost.localdomain>
2019-11-11 21:19:09 -0700

    * color incorporates the ability to parse "rgb: #,#,#" values and returns a bool   value if found (true), or not (false)

--- color/color.go
+++ color/color.go
@@ -1,5 +1,11 @@
 package color
 
+import (
+  "fmt"
+  "regexp"
+  "strconv"
+)
+
 type RGBStruct struct {
   Red, Green, Blue int
 }
@@ -17,14 +23,32 @@ var ColorMap = map[string]RGBStruct{
   "red":        RGBStruct{255,   0,   0},
   "teal":       RGBStruct{  0, 128, 128},
   "yellow":     RGBStruct{255, 255,   0},
+  "off":        RGBStruct{  0,   0,   0},
   "black":      RGBStruct{  0,   0,   0},
+  "on":         RGBStruct{255, 255, 255},
   "white":      RGBStruct{255, 255, 255},
 }
 
 func Lookup(value string) (RGBStruct, bool) {
+  re := regexp.MustCompile("(?:rgb\\s*:\\s*)(\\d+),(\\d+),(\\d+)")
+
+  match := re.FindStringSubmatch(value)
+  if match != nil {
+    r, _ := strconv.Atoi(match[1])
+    g, _ := strconv.Atoi(match[2])
+    b, _ := strconv.Atoi(match[3])
+
+    return RGBStruct{Red: r, Green: g, Blue: b}, true
+  }
+
   if result, ok := ColorMap[value]; ok {
     return result, true
   }
 
   return RGBStruct{}, false
 }
+
+func (rgb RGBStruct) String() string {
+  return fmt.Sprintf("Red: %03d, Green: %03d, Blue: %03d", rgb.Red, rgb.Green, rgb.Blue)
+}
+
